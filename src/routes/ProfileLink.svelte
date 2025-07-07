b62a458c7e962fe303926d3eab206eb817772a9e
Rik <gitaarik@posteo.net>
2025-07-07 20:53:24 +0200

    --wip-- [skip ci]

--- src/routes/ProfileLink.svelte
+++ src/routes/ProfileLink.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
 
+  let elLink: HTMLElement;
+
   export let icon: object;
   export let href: string = "";
   export let title: string = "";
+
+  function handleClick() {
+    elLink.blur();
+  }
 </script>
 
 <a
@@ -11,6 +17,8 @@
   target="_blank"
   {title}
   class="text-l text-5xl hover:text-[var(--highlight-color)] focus:text-[var(--highlight-color)] transition"
+  bind:this={elLink}
+  on:click={handleClick}
 >
   <FontAwesomeIcon
     {icon}
