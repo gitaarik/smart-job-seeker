b97c29cd77945316a7b7a906e504c69242d151e9
Rik <gitaarik@posteo.net>
2025-07-09 23:10:11 +0200

    --wip-- [skip ci]

--- src/lib/images/Logo.svelte
+++ src/lib/images/Logo.svelte
@@ -12,13 +12,13 @@
   <g transform="translate(-101.76 -135.84)">
     <g transform="translate(-169.65 163.8)">
       <path
-        class="scale-100 hover:scale-103 hover:-translate-10 transition duration-500"
+        class="scale-100 hover:scale-105 hover:-translate-15 transition duration-500"
         transform="matrix(.13333 0 0 -.13333 0 5333.3)"
         d="m7317.4 31709h-1617.8l-2059.2 3567.1 2059.2 3567.2h1617.8l-2060-3567.2zm-2435.3 8500.5-2555.9-4429.4-290.7-504 290.7-503.2 2548.5-4415.6h3060.7l1331.3 3803.3 2117 6048.9h-6501.6"
       >
       </path>
       <path
-        class="scale-100 hover:scale-103 hover:-translate-10 transition duration-500"
+        class="scale-100 hover:scale-105 hover:-translate-15 transition duration-500"
         transform="matrix(.13333 0 0 -.13333 0 5333.3)"
         d="m14568 29809h-1617.9l2060 3567.2-2060 3567.1h1617.9l2058.4-3567.1zm3373.3 4069.6-2548.5 4416.4h-3060.7l-1331.3-3803.3-2117-6048.9h6500.9l2556.6 4429.4 290.7 504-290.7 502.4"
       >
