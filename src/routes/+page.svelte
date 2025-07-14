a801fbbce1d5c09f4ff471bf03b625b2bdc33f88
Rik <gitaarik@posteo.net>
2025-07-14 12:26:17 +0200

    --wip-- [skip ci]

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -63,22 +63,22 @@
 <svelte:head>
   <title>Rik Wanders - Freelance Full Stack Developer</title>
 
-  <meta name="title" content={metaTitle} />
-  <meta name="description" content={metaDescription} />
+  <meta name="title" content={metaTitle}>
+  <meta name="description" content={metaDescription}>
 
   <!-- Open Graph / Facebook -->
-  <meta property="og:type" content="website" />
-  <meta property="og:url" content={metaUrl} />
-  <meta property="og:title" content={metaTitle} />
-  <meta property="og:description" content={metaDescription} />
-  <meta property="og:image" content={metaImg} />
+  <meta property="og:type" content="website">
+  <meta property="og:url" content={metaUrl}>
+  <meta property="og:title" content={metaTitle}>
+  <meta property="og:description" content={metaDescription}>
+  <meta property="og:image" content={metaImg}>
 
   <!-- X (Twitter) -->
-  <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content={metaUrl} />
-  <meta property="twitter:title" content={metaTitle} />
-  <meta property="twitter:description" content={metaDescription} />
-  <meta property="twitter:image" content={metaImg} />
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content={metaUrl}>
+  <meta property="twitter:title" content={metaTitle}>
+  <meta property="twitter:description" content={metaDescription}>
+  <meta property="twitter:image" content={metaImg}>
 </svelte:head>
 
 <main class="flex h-full min-h-screen flex-col justify-between">
