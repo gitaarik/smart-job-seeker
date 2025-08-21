32e634a8cfff46f9489dae6d242a9448b2a4613f
Rik <gitaarik@posteo.net>
2025-08-21 15:05:43 +0200

    --wip-- [skip ci]

--- src/routes/resume/ReferencesSection.svelte
+++ src/routes/resume/ReferencesSection.svelte
@@ -14,12 +14,25 @@
   <div class="space-y-4">
     {#each resume.references as reference (reference.name)}
       <div class="border-l-4 border-ocean pl-4 py-2">
-        <h4 class="font-semibold text-base print:text-sm">
-          {reference.name}
-        </h4>
-        <p class="text-sm mt-1">
-          {reference.reference}
-        </p>
+        <div class="flex items-start justify-between">
+          <div class="flex-1">
+            <h4 class="font-semibold text-base print:text-sm">
+              {reference.name}
+            </h4>
+            <p class="text-sm mt-1">
+              {reference.reference}
+            </p>
+          </div>
+          {#if reference.logo}
+            <div class="ml-4 flex-shrink-0">
+              <img
+                src={reference.logo}
+                alt="{reference.name} Logo"
+                class="h-20 w-auto border border-agua rounded"
+              />
+            </div>
+          {/if}
+        </div>
       </div>
     {/each}
   </div>
