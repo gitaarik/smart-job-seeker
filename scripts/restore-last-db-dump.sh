03ad5ca3f0e877cc3d19f53546a56b04a871cba5
Rik <gitaarik@posteo.net>
2025-10-21 18:04:17 +0200

    --wip-- [skip ci]

--- scripts/restore-last-db-dump.sh
+++ scripts/restore-last-db-dump.sh
@@ -6,9 +6,15 @@ read -p "This erases current database. Continue?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+
+  # Force disconnect users, so we can drop the database
   psql -U postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'smartjobseeker' AND pid <> pg_backend_pid()"
+
+  # Drop & recreate the database
   psql -U postgres -c "DROP DATABASE smartjobseeker"
   psql -U postgres -c "CREATE DATABASE smartjobseeker"
+
+  # Import the latest dump
   psql -U postgres -d smartjobseeker < latest.sql
 fi
 
