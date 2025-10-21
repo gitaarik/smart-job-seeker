45b8920f460e7f805836ca5fe715483da94b444f
Rik <gitaarik@posteo.net>
2025-10-21 18:02:12 +0200

    --wip-- [skip ci]

--- scripts/restore-last-db-dump.sh
+++ scripts/restore-last-db-dump.sh
@@ -6,6 +6,9 @@ read -p "This erases current database. Continue?" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  psql -U postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'smartjobseeker' AND pid <> pg_backend_pid()"
+  psql -U postgres -c "DROP DATABASE smartjobseeker"
+  psql -U postgres -c "CREATE DATABASE smartjobseeker"
   psql -U postgres -d smartjobseeker < latest.sql
 fi
 
