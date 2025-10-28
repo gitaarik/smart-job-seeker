7eb26979d259f95f91ce65e75fe14166a4692861
Rik <gitaarik@posteo.net>
2025-10-28 19:45:20 +0100

    --wip-- [skip ci]

--- src/routes/cv/ProfileSection.svelte
+++ src/routes/cv/ProfileSection.svelte
@@ -3,6 +3,7 @@
   import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
   import {
     faEnvelope,
+    faFlag,
     faGlobe,
     faLanguage,
     faMapMarkerAlt,
@@ -21,39 +22,62 @@
   const linkedInProfile = profile.linkedin_profile;
 
   const profileData = [
-    { icon: faMapMarkerAlt, text: profile.location },
-    { icon: faLanguage, text: languageText },
+    {
+      icon: faMapMarkerAlt,
+      text: profile.location,
+      label: "Location"
+    },
+
+    {
+      icon: faLanguage,
+      text: languageText,
+      label: "Language"
+    },
+
+    {
+      icon: faFlag,
+      text: profile.nationality,
+      label: "Nationality"
+    },
+
     {
       icon: faPhone,
       text: profile.phone_number,
       href: `tel:${profile.phone_number}`,
+      label: "Phone number"
     },
+
     {
       icon: faEnvelope,
       text: profile.email_address,
       href: `mailto:${profile.email_address}`,
+      label: "Email address"
     },
+
     {
       icon: faGlobe,
       text: profile.personal_website.replace("https://", ""),
       href: profile.personal_website,
       target: "_blank",
+      label: "Website"
     },
+
     {
       icon: faLinkedin,
       text: linkedInProfile?.replace("https://www.", "") || "",
       href: linkedInProfile || "",
       target: "_blank",
+      label: "LinkedIn"
     },
   ];
 </script>
 
 <InfoSection title="Profile" icon={faUser}>
   <ul class="columns-2 gap-6">
-    {#each profileData as item (item.icon)}
-      <li class="flex items-start mb-4 break-inside-avoid">
+    {#each profileData as item (item.label)}
+      <li class="flex items-start mb-4 break-inside-avoid" aria-label={item.label}>
         <div class="mr-2 flex-shrink-0">
-          <FontAwesomeIcon icon={item.icon} class="w-4 h-4 mt-1" />
+          <FontAwesomeIcon icon={item.icon} class="w-4 h-4 mt-1" title={item.label} />
         </div>
         <p>
           {#if item.href}
@@ -61,6 +85,7 @@
               href={item.href}
               class="underline text-slate hover:text-teal"
               target={item.target || ""}
+              title={item.label}
             >
               {item.text}
             </a>
