5c92258675f6debe4b30ccb536341d22720febd3
Rik <gitaarik@posteo.net>
2025-11-02 20:55:02 +0100

    --wip-- [skip ci]

---
+++ prisma/migrations/1762112155_rename_years_experience_to_years_experience2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "tech_skills" RENAME COLUMN "years_experience" TO "years_experience2";
---
+++ prisma/migrations/1762112179_remove_years_experience2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "tech_skills" DROP COLUMN "years_experience2";
